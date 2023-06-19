47984d7cfea8aacfe2466fac18fe3fe7ec4c3775
[Francisco Omedes] <[fomedes@uoc.edu]>
2023-06-19 22:02:17 +0100

    sale controller debug

--- app/Http/Controllers/MarketSaleController.php
+++ app/Http/Controllers/MarketSaleController.php
@@ -36,13 +36,77 @@ class MarketSaleController extends Controller
                   'move_type' => 'sale'
               ];
 
-              // Store the sold player data
-              //Transfer::insert($soldPlayer);
-              return response()->json(['message' => $soldPlayer], 200);
+                // Store the sold player data
+                Transfer::insert($soldPlayer);
+
+                // Add sold player amount to funds
+                $this->addFunds($soldPlayer);
+                // Remove player from user
+                $this->removePlayerFromClub($soldPlayer);
+                // Mark player as Agente Libre
+                $this->updatePlayerCurrentClub($soldPlayer);
+                // Mark player as not a free agent
+                $this->markPlayerFreeAgent($soldPlayer);
+              
+
+              return response()->json(['message' => 'Player sold successfully'], 200);
 
           }
         }
-       // return response()->json(['message' => 'Player not found'], 404);
+        return response()->json(['message' => 'Player not found'], 404);
     }    
+
+    private function addFunds($soldPlayer)
+    {
+        $sellingClub = $soldPlayer['club'];
+        $sellingAmount = $soldPlayer['amount'];
+
+        $user = User::where('userId', $sellingClub)->first();
+        if ($user) {
+            $user->funds += $sellingAmount;
+            $user->save();
+        }
+    }
+
+    private function removePlayerFromClub($soldPlayer)
+    {
+        $sellingClub = $soldPlayer['club'];
+        $playerId = $soldPlayer['player_id'];
+
+        $user = User::where('userId', $sellingClub)->first();
+        if ($user) {
+            $clubPlayers = json_decode($user->club_players, true);
+            $index = array_search($playerId, $clubPlayers);
+        
+            if ($index !== false) {
+                array_splice($clubPlayers, $index, 1);
+            }
+
+            $user->club_players = json_encode($clubPlayers);
+            $user->save();
+        }
+    }
+
+    private function updatePlayerCurrentClub($soldPlayer)
+    {
+        $playerId = $soldPlayer['player_id'];
+
+        $player = Player::find($playerId);
+        if ($player) {
+            $player->current_club = 'Agente Libre';
+            $player->save();
+        }
+    }
+
+    private function markPlayerFreeAgent($soldPlayer)
+    {
+        $playerId = $soldPlayer['player_id'];
+
+        $player = Player::find($playerId);
+        if ($player) {
+            $player->isFreeAgent = true;
+            $player->save();
+        }
+    }
 
 }
